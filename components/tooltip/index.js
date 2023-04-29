ea91309daf8c3e6757eb4b77c3cd87fa768c0273
Hamza Khalid <hamza.khalid@stixor.com>
2023-04-29 20:23:10 +0500

    changes

--- components/tooltip/index.js
+++ components/tooltip/index.js
@@ -1,32 +1,66 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classes from "./tooltip.module.css";
 import logo from "../../public/assets/toolbar/logo_white.svg";
 import consultant from "../../public/assets/toolbar/consultant.svg";
 import expert from "../../public/assets/toolbar/expert.svg";
 
 function Tooltip() {
+  const [height, setHeight] = useState("82px");
+  const tooltipRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    const tooltipBody = tooltipRef.current.querySelector(
+      `.${classes.icons_body}`
+    );
+    const height = tooltipBody.getBoundingClientRect().height + 20; // add 20 for padding
+    setHeight(`${height}px`);
+    console.log(height);
+  };
+
+  const handleMouseLeave = () => {
+    setHeight("82px");
+  };
+
   return (
-    <div className={classes.tooltip_container}>
+    <div
+      className={classes.tooltip_container}
+      style={{ height: height }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      ref={tooltipRef}
+    >
       <div className={classes.tooltip_body}>
         <img src={logo.src} />
       </div>
-      <div className={classes.single_icon_container}>
-        <img src={consultant.src} />
-        <p>
-          GROUP <br />
-          TOURS
-        </p>
-      </div>
-      <div className={classes.single_icon_container}>
-        <img src={expert.src} />
-        <p>EXPERIENCES</p>
-      </div>
-      <div className={classes.single_icon_container}>
-        <img src={consultant.src} />
-        <p>
-          GROUP <br />
-          TOURS
-        </p>
+      <div className={classes.icons_body}>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 1</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 2</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 3</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 3</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 3</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 3</p>
+        </div>
+        <div className={classes.single_icon_container}>
+          <img src={consultant.src} />
+          <p>Heading 3</p>
+        </div>
       </div>
     </div>
   );
